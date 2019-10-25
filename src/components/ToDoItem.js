67d212c8b5259f64a6c70c784b0ad8abcddf84d1
Liam Dawson <liamrdawson@gmail.com>
2019-10-25 21:01:40 +0100

    add PropTypes to ToDos and ToDoItem

---
+++ src/components/ToDoItem.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export class ToDoItem extends Component {
+    render() {
+        return (
+            <div>
+                <h3>{this.props.todo.title}</h3>
+            </div>
+        )
+    }
+}
+
+//  PROPTYPES
+ToDoItem.propTypes = {
+    todo: PropTypes.array.isRequired
+}
+
+export default ToDoItem
--- src/components/ToDos.js
+++ src/components/ToDos.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react'
 
+import ToDoItem from './ToDoItem';
+import PropTypes from 'prop-types';
+
 export default class ToDos extends Component {
     render() {
 
         console.log(this.props.todos)
         return this.props.todos.map((todo) => (
-            <h3>{todo.title}</h3>
+            <ToDoItem key={todo.id} todo={todo}/>
         ));
     }
+}
+
+//  PROPTYPES
+ToDos.propTypes = {
+    ToDos: PropTypes.array.isRequired
 }
